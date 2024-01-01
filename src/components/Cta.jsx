92a7f6a69a00ae5e7e17c1030524ef6656589f2c
ali-mashayekhi <alimashayekhi42@gmail.com>
2024-01-01 10:19:58 +0330

    cta desktop design completed

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,16 +1,13 @@
 import Button from "./UI/Button";
 
-import bgPattern from "../assets/bg-boost-mobile.svg";
+import "./Cta.css";
 
 function Cta() {
   return (
     <section className="bg-gray-transparent-custom sm:pt-[3.75rem]">
       <div>
-        <div
-          className="flex flex-col items-center bg-dark-violet-custom px-2 pb-[5.375rem] pt-24"
-          style={{ background: `url(${bgPattern}), hsl(257, 27%, 26%)` }}
-        >
-          <h2 className="mb-3 bg-no-repeat text-[1.75rem] font-bold tracking-tight text-white">
+        <div className="bg-pattern flex flex-col items-center bg-dark-violet-custom px-2 pb-[5.375rem] pt-24 sm:py-[3.4375rem] ">
+          <h2 className="mb-3 bg-no-repeat text-[1.75rem] font-bold tracking-tight text-white sm:mb-5 sm:text-[2.5rem]">
             Boost your links today
           </h2>
           <Button btnHref="#">Get Started</Button>
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ function Button(props) {
   return (
     <a
       href={props.btnHref}
-      className="flex  h-[3.5625rem] w-[12.3125rem] items-center  justify-center rounded-full bg-cyan-custom text-lg font-semibold text-white"
+      className="flex h-[3.5625rem] w-[12.3125rem] items-center  justify-center rounded-full bg-cyan-custom text-lg font-semibold text-white"
     >
       {props.children}
     </a>
